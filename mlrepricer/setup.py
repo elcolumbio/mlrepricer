e43f890ef4a5839f7cd585e4cb3ee76ff1d508df
Florian Benkö <florian.benkoe@uni-konstanz.de>
2018-05-12 23:02:41 +0200

    path to config

--- mlrepricer/setup.py
+++ mlrepricer/setup.py
@@ -4,8 +4,13 @@
 
 from ruamel.yaml import YAML
 
+path_to_config = None  # '/home/jeff/'
+
+if path_to_config is None:
+    raise ValueError('path_to_config canot be empty')
+
 yaml = YAML(typ='unsafe')
 yaml.default_flow_style = False
 
-with open('./configs.yaml', 'r') as f:
+with open(path_to_config, 'r') as f:
     configs = yaml.load(f)
