be607de5f9696e3ea60a4910494e7be11423c168
Florian Benkö <florian.benkoe@uni-konstanz.de>
2018-06-19 04:36:53 +0200

    cleanup and normalization

--- mlrepricer/helper.py
+++ mlrepricer/helper.py
@@ -3,6 +3,7 @@
 import pandas as pd
 from time import sleep
 import mws
+import numpy as np
 
 from . import setup
 
@@ -38,6 +39,35 @@ def load_dataframe(foldername):
     """Load pandas df from storage."""
     with open(datafolder+foldername, 'rb') as f:
         return pd.read_msgpack(f)
+
+
+def cleanup(df):
+    """Input the complete data and remove very bad listings in the output."""
+    # we should calculate the winners we dropped.
+    f1 = df[(df.instock == 1) & (df.isfeaturedmerchant == 1)]
+    f1 = f1.drop(['instock', 'isfeaturedmerchant'], axis=1)
+    # those offers suck too
+    f1 = f1[(f1.shipping_maxhours+f1.shipping_minhours) <= 72]
+    f1 = f1[(f1.feedback > 20) | (f1.isprime == 1)]
+    return f1
+
+
+def normalize(f1):
+    """Take output from cleanup and scales and merge data."""
+    # normalize the feedback count to a number between 0 and 1
+    f1['feedback'] = np.where(np.log(f1.feedback+1)/10 <= 1,
+                              np.log(f1.feedback+1)/10, 1)
+    # normalize the feedbackpercent to a number between 0 and 1
+    f1['feedbackpercent'] = (f1.feedbackpercent/100)**2
+    # combine max and minshipping time to a number between 0 and 1
+    f1['shipping_time'] = (f1.shipping_maxhours+f1.shipping_minhours)
+    # one hot encode shipping time, there should be only 3-10 unique times
+    f1 = pd.get_dummies(f1, prefix='shipping_time', columns=['shipping_time'])
+    f1 = f1.drop(['shipping_maxhours', 'shipping_minhours'], axis=1)
+    # linear transformation of none prime prices
+    f1['price'] = np.where((f1.isprime == 0), f1.price*0.859-0.567, f1.price)
+    f1 = f1.drop(['isprime'], axis=1)
+    return f1
 
 
 def auto_get_report_id(request, rec_level=0):
